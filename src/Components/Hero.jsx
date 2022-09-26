da67556e03a6c8b5fd921e5c968b5e9da9aed063
Saif045 <saif.x45@gmail.com>
2022-09-27 00:08:36 +0200

    sda

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ const hero = () => {
     <div className='sm:order-first flex justify-center' >
       <div className='m-4 my-16 sm:my-0 w-4/4 xs:w-3/4 sm:w-4/4 md:w-3/4 lg:w-3/5 text-center md:text-left' >
         <div className=' font-extrabold text-3xl md:text-4xl '>Transform your brand</div>
-        <div className=' py-8 sm:py-4 leading-snug  text-gray-500 text-sm xs:text-base sm:text-sm lg:text-base'>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</div>
+        <div className=' py-8 sm:py-4 leading-snug  text-gray-500 text-sm xs:text-base sm:text-sm lg:text-base lg:py-6 '>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</div>
         <div className='font-black font-serif text-sm underline decoration-yellow-200 decoration-8 underline-offset-1  '>LEARN MORE</div>      
        </div>
      </div>
@@ -22,7 +22,7 @@ const hero = () => {
       <div className='flex justify-center' >
       <div className='m-4 my-16 sm:my-0 w-4/4 xs:w-3/4 sm:w-4/4 md:w-3/4 lg:w-3/5 text-center md:text-left' >
         <div className=' font-extrabold text-3xl md:text-4xl '>Stand out to the right audience</div>
-        <div className=' py-8 sm:py-4 leading-snug text-gray-500 text-sm xs:text-base sm:text-sm lg:text-base'>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.</div>
+        <div className=' py-8 sm:py-4 leading-snug text-gray-500 text-sm xs:text-base sm:text-sm lg:text-base lg:py-6'>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.</div>
         <div className='font-black font-serif text-sm underline decoration-red-200 decoration-8 underline-offset-1   '>LEARN MORE</div>      
       </div>
       </div>
